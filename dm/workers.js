a7d714ba298867b042d0e528c41d2052d97b7b24
2091k <140614254+2091k@users.noreply.github.com>
2024-09-01 10:42:02 +0800

    Update workers.js
    Signed-off-by: 2091k <140614254+2091k@users.noreply.github.com>

--- dm/workers.js
+++ dm/workers.js
@@ -1,130 +1,89 @@
+const upstream = 'raw.githubusercontent.com';
+const upstream_mobile = 'raw.githubusercontent.com';
+const blocked_region = ['RU'];
+const blocked_ip_address = ['0.0.0.0', '127.0.0.1'];
+
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+    event.respondWith(fetchAndApply(event.request));
+});
 
-function parseURL(url) {
-    let urlbody = url.substr(8);
-    urlbody = decodeURIComponent(urlbody.substr(urlbody.indexOf('/') + 1))
-    console.log("    Parsing: URLBody: " + urlbody)
-    let split_header_url = urlbody.lastIndexOf("/", urlbody.search("://"))
-    let real_url = urlbody.substr(split_header_url + 1)
-    if (!real_url) {
-        throw "Invalid real URL: " + urlbody
-    }
-    let headersbody = urlbody.substr(0, split_header_url)
-    console.log("    Parsing: Real URL: " + real_url)
-    console.log("    Parsing: Headers JSON: " + headersbody)
-    if (!headersbody) {
-        return {
-            url: real_url,
-            headers: {},
-        }
-    }
-    if (!headersbody.startsWith("{")) {
-        headersbody = decodeURIComponent(headersbody)
+async function fetchAndApply(request) {
+    const region = request.headers.get('cf-ipcountry').toUpperCase();
+    const ip_address = request.headers.get('cf-connecting-ip');
+    const user_agent = request.headers.get('user-agent');
+
+    let url = new URL(request.url);
+
+    if (url.protocol === 'http:') {
+        url.protocol = 'https:';
+        return Response.redirect(url.href);
     }
 
-    if (!headersbody.startsWith("{")) {
-        throw "Invalid URL headers string: " + headersbody
+    if (blocked_region.includes(region)) {
+        return new Response('Access denied: WorkersProxy is not available in your region yet.', {
+            status: 403
+        });
     }
 
-    headers = JSON.parse(headersbody)
-    
-    return {
-        url: real_url,
-        headers: headers,
+    if (blocked_ip_address.includes(ip_address)) {
+        return new Response('Access denied: Your IP address is blocked by WorkersProxy.', {
+            status: 403
+        });
     }
+
+    const upstream_domain = await device_status(user_agent) ? upstream : upstream_mobile;
+    url.host = upstream_domain;
+
+    const new_request_headers = new Headers(request.headers);
+    new_request_headers.set('Host', upstream_domain);
+    new_request_headers.set('Referer', url.href);
+
+    const original_response = await fetch(url.href, {
+        method: request.method,
+        headers: new_request_headers
+    });
+
+    const content_type = original_response.headers.get('content-type');
+    let response_body;
+
+    // 只替换HTML页面内容，其它类型内容保持不变
+    if (content_type && content_type.includes('text/html')) {
+        response_body = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Welcome to nginx!</title>
+            <style>
+                body {
+                    width: 35em;
+                    margin: 0 auto;
+                    font-family: Tahoma, Verdana, Arial, sans-serif;
+                }
+            </style>
+        </head>
+        <body>
+        <h1>Welcome to nginx!</h1>
+        <p>If you see this page, the nginx web server is successfully installed and working. Further configuration is required.</p>
+        <p>For online documentation and support please refer to <a href="http://nginx.org/">nginx.org</a>.<br/>
+        Commercial support is available at <a href="http://nginx.com/">nginx.com</a>.</p>
+        <p><em>Thank you for using nginx.</em></p>
+        </body>
+        </html>`;
+    } else {
+        response_body = original_response.body; // 非HTML内容不做修改
+    }
+
+    const new_response_headers = new Headers(original_response.headers);
+    new_response_headers.set('access-control-allow-origin', '*');
+    new_response_headers.set('access-control-allow-credentials', 'true');
+
+    return new Response(response_body, {
+        status: original_response.status,
+        headers: new_response_headers
+    });
 }
 
-async function handleRequest(request) {
-    if (request.method == "OPTIONS") {
-        return new Response("", {status:200, headers:{
-            "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Max-Age": "31536000",
-            "X-Request-Type": "CORS Preflight"
-        }});
-    }
-    
-    let reqHeaders = new Headers(request.headers),
-        outBody, outStatus = 200, outCt = null, outHeaders = new Headers({
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
-        });
-
-    try {
-        console.log("Got Raw ReqUrl: " + request.url)
-        let t = parseURL(request.url)
-        let url = t.url
-        let headers = t.headers
-        console.log("Parsed URL body: " + url)
-        console.log("Parsed URL headers: " + JSON.stringify(headers))
-
-        if (url.length < 3 || url.indexOf('.') == -1) {
-            throw "invalid URL input: " + url;
-        } else if (url == "favicon.ico" || url == "robots.txt") {
-            return Response.redirect('https://workers.cloudflare.com', 307)
-        } else {
-            if (url.toLowerCase().indexOf("http") == -1) {
-                url = "http://" + url;
-            }
-
-            let fp = {
-                method: request.method,
-                headers: {}
-            }
-
-            let he = reqHeaders.entries();
-            for (let h of he) {
-                if (!['content-length', 'content-type'].includes(h[0])) {
-                    fp.headers[h[0]] = h[1];
-                }
-            }
-
-            if (headers["_method"] !== undefined) {
-                fp.method = headers["_method"].toUpperCase()
-            }
-            fp.headers = Object.assign({}, fp.headers, headers)
-            
-            if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
-                const ct = (reqHeaders.get('content-type') || "").toLowerCase();
-                if (ct.includes('application/json')) {
-                    fp.body = JSON.stringify(await request.json());
-                } else if (ct.includes('application/text') || ct.includes('text/html')) {
-                    fp.body = await request.text();
-                } else if (ct.includes('form')) {
-                    fp.body = await request.formData();
-                } else {
-                    fp.body = await request.blob();
-                }
-            }
-            if (headers["_body"] !== undefined) {
-                fp.body = headers["_body"]
-            }
-
-            let fr = (await fetch(url, fp));
-            outStatus = fr.status;
-            outCt = fr.headers.get('content-type');
-            outBody = fr.body;
-        }
-    } catch (err) {
-        outStatus = 500
-        outCt = "application/json";
-        outBody = JSON.stringify({
-            code: -1,
-            msg: JSON.stringify(err.stack) || err
-        });
-    }
-
-    if (outCt && outCt != "") {
-        outHeaders.set("content-type", outCt);
-    }
-
-    return new Response(outBody, {
-        status: outStatus,
-        headers: outHeaders
-    })
+async function device_status(user_agent_info) {
+    const agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    return !agents.some(agent => user_agent_info.includes(agent));
 }
